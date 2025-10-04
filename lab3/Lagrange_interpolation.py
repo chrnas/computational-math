b69550980253eaf0732edc06a6c655a9b43353a1
Astrid Pettersson <astpe354@student.liu.se>
2025-10-04 13:25:50 +0200

    think im done with 3.2

--- lab3/Lagrange_interpolation.py
+++ lab3/Lagrange_interpolation.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def Lagrange_interpolation(z, ???):
+def Lagrange_interpolation(z, data_points):
 #
 #  Implementation to evaluate an interpolating polynomial p_n(x)
 #  at the point x = z. The polynomial uses the standard Lagrange
@@ -9,7 +9,7 @@ def Lagrange_interpolation(z, ???):
 #  INPUT:
 #
 #     z    - 1x1 value to evaluate
-#     ???
+#     data_points - list of (x, y) points
 #
 #  OUTPUT:
 #
@@ -17,13 +17,19 @@ def Lagrange_interpolation(z, ???):
 #
 
 #  Compute the polynomial interpolation sum evaluated at x = z
-   pnx =
-   for i in
+   pnx = 0 
+   n = len(data_points)
+   for i in range(n):
+      temp = 1
+      for j in range(n):
+         if i == j:
+            continue
+         temp = temp * (z - data_points[j][0]) / (data_points[i][0]-data_points[j][0])
+         #print(temp)
 
 
-      for j in
 
 
-      pnx =
+      pnx += data_points[i][1]*temp
 
    return pnx
---
+++ lab3/lab3_2.py
@@ -0,0 +1,42 @@
+from Lagrange_interpolation import Lagrange_interpolation
+import numpy as np
+import matplotlib.pyplot as plt
+datapoints = [[(-1,2), (0,0), (1,2)],[(-1,-6), (0,-3), (1,0), (1.5,3.375)]]
+ 
+
+def build_Interpolation(datapoints, M):
+    X = np.linspace(datapoints[0][0], datapoints[-1][0], M)
+    L = np.zeros((M,))
+    for k in range(M):
+        L[k] = Lagrange_interpolation(X[k], datapoints)
+    return X,L
+
+L =[]
+X = []
+for dp in datapoints:
+    X_curve, L_curve = build_Interpolation(dp, M=150)
+    L.append(L_curve)
+    X.append(X_curve)
+    x_data = [p[0] for p in dp]
+    y_data = [p[1] for p in dp]
+
+    plt.figure(figsize=(8, 6)) # 
+    plt.plot(X_curve, L_curve, label='Lagrange Polynomial', color='blue') 
+    plt.scatter(x_data, y_data, color='red', zorder=5, label='Data Points') 
+
+    plt.title('Lagrange Interpolation')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True) 
+    plt.legend() 
+
+
+    plt.show()
+
+
+func1 = lambda x: 2*x**2
+func2 = lambda x: x**3 + 2*x - 3
+
+print(np.linalg.norm(func1(X[0])-L[0], ord=np.inf))
+
+print(np.linalg.norm(func1(X[1])-L[1], ord=np.inf))
