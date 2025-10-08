909c9e0988ed1689ab0a4ef3871e4fbac293f497
Astrid Pettersson <astpe354@student.liu.se>
2025-10-08 13:49:52 +0200

    almost done with 4.4

--- lab4/RungeKutta4.py
+++ lab4/RungeKutta4.py
@@ -12,8 +12,14 @@ def RungeKutta4(f, t0, tn, y0, h):
 
 #  use forward Euler with the given time step h to solve the problem and
 #  save the information for later analysis
+   
    for j in range(1,len(t)):
 #  this is an explicit method so we always use values from the past solution history
-
+      k1 =f(t[j-1], y[j-1])
+      k2 = f(t[j-1]+h/2, y[j-1]+k1*(h/2))
+      k3 = f(t[j-1]+h/2, y[j-1]+k2*(h/2))
+      k4 = f(t[j-1]+h, y[j-1]+h*k3)
+      
+      y[j]=y[j-1] + (h/6)*(k1 + 2*k2 + 2*k3 +k4)
 
    return t, y
