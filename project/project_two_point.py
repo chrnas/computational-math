6b2ca8bc6020ed73b8ce13859cf8ccb78cb82cfd
Astrid Pettersson <astpe354@student.liu.se>
2025-10-13 20:54:02 +0200

    dont know if correct but seems reasonable, will work on plotting cpu timings tomorrow

--- project/project_two_point.py
+++ project/project_two_point.py
@@ -1,6 +1,6 @@
 import numpy as np
 from project_thomas import thomas
-
+import matplotlib.pyplot as plt
 
 def two_point(a,b, N):
     h = (b-a)/N
@@ -77,15 +77,29 @@ N = 20
 
 #print(y_exact)
 #print(thomas(A,S))
+ratios=[]
+Ns = [5,10, 20,25, 30,35, 40, 50, 60, 70, 80]
+#Ns= [10,20,40,80]
 temp = None
-for i in [5, 10, 20, 40, 80]:
+for i in Ns:
     A, rhs, y_exact = two_point2(a, b, i)
-    print(thomas(A,rhs))
-    print(y_exact)
-    break
+    #print(thomas(A,rhs))
+    #print(y_exact)
+    
     inf_norm = np.linalg.norm(thomas(A,rhs)-y_exact, ord=np.inf)
     print("inf norm", inf_norm, i)
-    if temp != None:
+    if temp == None:
+        ratios.append(None) # append so we can plot this is does not mean anything
+    else:
         error_ratio = temp/inf_norm
+        ratios.append(error_ratio)
         print("ratio",error_ratio)
     temp = inf_norm
+
+
+plt.plot(Ns, ratios)
+plt.xlabel('N', fontsize=12)
+plt.ylabel('error ratio', fontsize=12)
+plt.legend()
+plt.grid(True)
+plt.show()
